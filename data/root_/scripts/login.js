0fa58efad7f9aefb43e8a22f7f3f47b937a9ad88
Super Zombi <rost201541@gmail.com>
2022-03-11 20:28:15 +0200

    Add files via upload

---
+++ data/root_/scripts/login.js
@@ -0,0 +1,88 @@
+function passwordToggle(what){
+	var input = what.parentNode.getElementsByTagName("input")[0]
+	if (input.type === "password") {
+		input.type = "text";
+		what.classList.add("fa-eye-slash");
+		what.title = "Hide";
+	}
+	else {
+		input.type = "password";
+		what.classList.remove("fa-eye-slash");
+		what.title = "Show";
+	}
+}
+
+function confirmPassword(main, child){
+	if(main.value != child.value) {
+		child.setCustomValidity("Passwords Don't Match");
+		child.reportValidity();
+		main.onchange = _=> child.setCustomValidity('');
+		child.onkeydown = _=> child.setCustomValidity('');
+		return false;
+	}
+	else {
+		child.setCustomValidity('');
+		return true;
+	}
+}
+
+function action_(){
+	var type = document.querySelector('input[name="form_action"]:checked').value;
+	var form = document.querySelector(`form[name="${type}"]`);
+
+	if (form.querySelector('input[name="confirm_password"]')){
+		if (
+			confirmPassword(
+				form.querySelector('input[name="password"]'),
+				form.querySelector('input[name="confirm_password"]')
+			)
+		)
+		{
+			parseForm(type, form)
+		}
+	}
+	else{
+		parseForm(type, form)
+	}
+}
+
+function parseForm(type, form){
+	var elements = Array.from(form.elements).filter(e => e.tagName.toLowerCase() != "button");
+	elements = elements.filter(e => e.value);
+	var final = {}
+	elements.map(e => {
+		if (e.name != "confirm_password"){
+			final[e.name] = e.value;
+		}
+	})
+	console.log(type);
+	console.log(final);
+}
+
+window.onload = function(){
+	var rad = document.querySelectorAll('input[name="form_action"]');
+	for (var i = 0; i < rad.length; i++) {
+		rad[i].addEventListener('change', function() {
+			if (this.value == "signup"){
+				document.querySelector(".flip-card").style.transform = "rotateY(180deg)"
+				document.querySelector(".flip-card-front").style.display = "none"
+				document.querySelector(".flip-card-back").style.display = "block"
+				location.hash = "signup"
+			}
+			else if(this.value == "login"){
+				document.querySelector(".flip-card").style.transform = "rotateY(0deg)"
+				document.querySelector(".flip-card-front").style.display = "block"
+				document.querySelector(".flip-card-back").style.display = "none"
+				location.hash = ""
+				history.replaceState("", "", location.pathname)
+			}
+		});
+	}
+	
+	if (window.location.hash.split('#')[1] == "signup"){
+		document.querySelector(".flip-card").style.transform = "rotateY(180deg)"
+		document.querySelector(".flip-card-front").style.display = "none"
+		document.querySelector(".flip-card-back").style.display = "block"
+		document.querySelectorAll('input[name="form_action"]')[1].checked = true
+	}
+}
---
+++ data/root_/scripts/theme.js
@@ -0,0 +1,164 @@
+/* Leaves */
+// (function leaves_(){
+//   if (typeof leaves_area !== 'undefined'){
+// 	var lea = document.createElement("link")
+// 	lea.rel = "stylesheet"
+// 	lea.setAttribute("href", "../../root_/styles/leaves.css");
+// 	document.head.appendChild(lea)
+// 	var scr = document.createElement("script")
+// 	scr.setAttribute("src", "../../root_/scripts/leaves.js");
+// 	document.head.appendChild(scr)   
+//   }
+//   else{
+//     setTimeout(function(){leaves_()}, 500)
+//   }
+// })()
+/**/
+
+/* Snowflakes */
+(function snowflakes(){
+	if (typeof leaves_area !== 'undefined'){
+		var lea = document.createElement("link")
+		lea.rel = "stylesheet"
+		lea.setAttribute("href", "../../root_/styles/snowfall.css");
+		document.head.appendChild(lea)
+		document.getElementById("leaves_area").innerHTML = `
+			<snowfall style="height:inherit;position:absolute;color:#43c7fa;">
+				${'<snowflake><span>❄</span></snowflake>'.repeat(20)}
+				${'<snowflake><span>•</span></snowflake>'.repeat(20)}
+				${'<snowflake><span>.</span></snowflake>'.repeat(10)}
+			</snowfall>
+		`
+		// lights()
+	}
+	else{
+		setTimeout(function(){snowflakes()}, 500)
+	}
+})()
+/**/
+
+/* Lights */
+// function lights(){
+// 		var lea = document.createElement("link")
+// 		lea.rel = "stylesheet"
+// 		lea.setAttribute("href", "../../root_/styles/lights.css");
+// 		document.head.appendChild(lea)
+// 		document.getElementById("leaves_area").innerHTML += `
+//         <ul class="line">
+//             <li class="red"></li>
+//             <li class="yellow"></li>
+//             <li class="blue"></li>
+//             <li class="pink"></li>
+//             <li class="red"></li>
+//             <li class="green"></li>
+//             <li class="blue"></li>
+//             <li class="yellow"></li>
+//             <li class="red"></li>
+//             <li class="pink"></li>
+//             <li class="blue"></li>
+//             <li class="yellow"></li>
+//             <li class="red"></li>
+//             <li class="green"></li>
+//             <li class="blue"></li>
+//             <li class="yellow"></li>
+//             <li class="red"></li>
+//             <li class="pink"></li>
+//             <li class="green"></li>
+//             <li class="blue"></li>
+//             <li class="pink"></li>
+//             <li class="red"></li>
+//             <li class="green"></li>
+//             <li class="blue"></li>
+//         </ul>
+// 		`
+// }
+/**/
+
+function change_switcher_title(){
+	try{document.getElementById("swicher").title = LANG.light}
+	catch{setTimeout(function(){change_switcher_title()}, 5)}
+}
+
+(function load_lang(){if (typeof LANG !== 'undefined'){
+
+var th = location.hash.split("#")[2]
+if (th){
+	if (th == "dark"){
+		darkThemeMq = true
+		var l = document.createElement("link")
+		l.rel = "stylesheet"
+		l.setAttribute("href", "../../root_/styles/dark.css");
+		l.setAttribute("id", "dark_file");
+		document.head.appendChild(l)
+		change_switcher_title()
+		window.localStorage.setItem('theme', 'dark');
+	}
+	else{
+		darkThemeMq = false
+		window.localStorage.setItem('theme', 'light');
+	}
+}
+else{
+	var tmp_th = window.localStorage.getItem('theme')
+	if(tmp_th){
+		if (tmp_th == "dark"){
+			darkThemeMq = true
+			var l = document.createElement("link")
+			l.rel = "stylesheet"
+			l.setAttribute("href", "../../root_/styles/dark.css");
+			l.setAttribute("id", "dark_file");
+			document.head.appendChild(l)
+			change_switcher_title()
+		}
+		else{
+			darkThemeMq = false
+		}
+	}
+	else{
+		darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)").matches
+		if (darkThemeMq) {
+			var l = document.createElement("link")
+			l.rel = "stylesheet"
+			l.setAttribute("href", "../../root_/styles/dark.css");
+			l.setAttribute("id", "dark_file");
+			document.head.appendChild(l)
+			change_switcher_title()
+			window.localStorage.setItem('theme', 'dark');
+		}
+		else{
+			window.localStorage.setItem('theme', 'light');
+		}
+	}
+}
+
+}else{setTimeout(function(){load_lang()}, 2)}})()
+
+
+function change_switcher(){
+	if (darkThemeMq){
+		document.getElementById("dark_file").remove()
+		document.getElementById("swicher").title = LANG.dark
+		location.hash = `#${language}#light`
+		window.localStorage.setItem('theme', 'light');
+	}
+	else{
+		var l = document.createElement("link")
+		l.rel = "stylesheet"
+		l.setAttribute("href", "../../root_/styles/dark.css");
+		l.setAttribute("id", "dark_file");
+		document.head.appendChild(l)
+		document.getElementById("swicher").title = LANG.light
+		location.hash = `#${language}#dark`
+		window.localStorage.setItem('theme', 'dark');
+	}
+	darkThemeMq = !darkThemeMq
+	setTimeout(function(){
+		arr = document.getElementById("links_area").getElementsByClassName("link")
+		Object.keys(arr).forEach(function(e){
+			var el = arr[e].getElementsByTagName("img")[0]
+			if (typeof el !== 'undefined'){
+				try_dark(el)
+			}
+		})
+	}, 300)
+}
