35af5612f13bfca3a02149c5313f5c46cc8f1c98
Super Zombi <rost201541@gmail.com>
2022-04-06 22:59:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -265,8 +265,11 @@ def reset():
 def register():
 	if "/" in request.json['name'] or "\\" in request.json['name']:
 		return jsonify({'successfully': False, 'reason': Errors.forbidden_character.name})
-	if len(request.json['name'].strip()) == 0:
-		return jsonify({'successfully': False, 'reason': Errors.forbidden_character.name})
+
+	for i in request.json.values():
+		if len(i.strip()) == 0:
+			return jsonify({'successfully': False, 'reason': Errors.forbidden_character.name})
+	
 	if request.json['name'].lower() == "admin":
 		return jsonify({'successfully': False, 'reason': Errors.name_already_taken.name})
 
