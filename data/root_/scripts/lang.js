a1e77d3485dda29934a1c2ee64632c84142b1408
Super Zombi <rost201541@gmail.com>
2022-04-08 21:18:24 +0300

    Update lang.js

--- data/root_/scripts/lang.js
+++ data/root_/scripts/lang.js
@@ -11,20 +11,20 @@ else{
 }
 
 (function executeIfFileExist() {
-	var src = `../../root_/Langs/${language.toUpperCase()}.json`;
+	var src = `/root_/Langs/${language.toUpperCase()}.json`;
 	var xhr = new XMLHttpRequest()
 	xhr.open('HEAD', src, true)
 	xhr.onreadystatechange = function() {
 		if (this.readyState === this.DONE) {
 			if (xhr.status==200){
 				var l = document.createElement("script")
-				l.setAttribute("src", `../../root_/Langs/${language.toUpperCase()}.json`);
+				l.setAttribute("src", `/root_/Langs/${language.toUpperCase()}.json`);
 				document.head.appendChild(l);
 				window.localStorage.setItem('lang', language);
 			}
 			else{
 				var l = document.createElement("script")
-				l.setAttribute("src", `../../root_/Langs/EN.json`);
+				l.setAttribute("src", `/root_/Langs/EN.json`);
 				document.head.appendChild(l);
 				var th = location.hash.split("#")[2]
 				if (typeof th === 'undefined'){
