ab955004230fb2fbb106e2ff5e6153ef2590f1b4
Super Zombi <rost201541@gmail.com>
2022-04-08 21:18:10 +0300

    Update lang_mini.js

--- data/root_/scripts/lang_mini.js
+++ data/root_/scripts/lang_mini.js
@@ -6,19 +6,19 @@ else{
 }
 
 (function executeIfFileExist() {
-	var src = `../root_/Langs/${language.toUpperCase()}.json`;
+	var src = `/root_/Langs/${language.toUpperCase()}.json`;
 	var xhr = new XMLHttpRequest()
 	xhr.open('HEAD', src, true)
 	xhr.onreadystatechange = function() {
 		if (this.readyState === this.DONE) {
 			if (xhr.status==200){
 				var l = document.createElement("script")
-				l.setAttribute("src", `../root_/Langs/${language.toUpperCase()}.json`);
+				l.setAttribute("src", `/root_/Langs/${language.toUpperCase()}.json`);
 				document.head.appendChild(l);
 			}
 			else{
 				var l = document.createElement("script")
-				l.setAttribute("src", `../root_/Langs/EN.json`);
+				l.setAttribute("src", `/root_/Langs/EN.json`);
 				document.head.appendChild(l);
 			}
 		}
