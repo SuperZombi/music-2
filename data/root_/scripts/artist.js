e011b6bfa6b2a5fe82d8ca1805d193753ec9cdb9
Super Zombi <rost201541@gmail.com>
2022-04-11 17:12:01 +0300

    Update artist.js

--- data/root_/scripts/artist.js
+++ data/root_/scripts/artist.js
@@ -158,15 +158,16 @@ function buildAbout(arr){
 		tr.appendChild(td2);
 		document.getElementById("about").getElementsByTagName("table")[0].appendChild(tr);
 
-		td1.innerHTML = e;
+		try{td1.innerHTML = LANG[e];}catch{td1.innerHTML = e;}
 		td2.innerHTML = arr[e];
+
 		if (e == "email"){
 			td1.insertAdjacentHTML("afterbegin", '<i class="fa-solid fa-envelope"></i>')
 			td2.innerHTML = `<a href="mailto:${arr[e]}">${arr[e]}</a>`
 		}
 		else if (e == "gender"){
 			td1.insertAdjacentHTML("afterbegin", '<i class="fa-solid fa-person"></i>')
-			td2.title = arr[e];
+			try{td2.title = LANG[arr[e]];}catch{td2.title = arr[e];}
 			if (arr[e] == "man"){
 				td2.innerHTML = '<i style="color:#00c0ff" class="fa-solid fa-mars"></i>'
 			}
