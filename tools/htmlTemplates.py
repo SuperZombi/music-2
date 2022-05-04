8e66c501e44b4b387e10e840562a0f977d142462
Super Zombi <rost201541@gmail.com>
2022-05-04 19:46:43 +0300

    Update htmlTemplates.py

--- tools/htmlTemplates.py
+++ tools/htmlTemplates.py
@@ -9,17 +9,19 @@ def track_index(artist, track, image):
 	<meta property="og:description" content="Zombi Music">
 	<meta property="og:site_name" content="zombi.music">
 	<meta property='og:type' content="music.song">
-	<script src="../../root_/scripts/lang.js"></script>
-	<script src="../../root_/scripts/theme.js"></script>
-	<link rel="stylesheet" href="../../root_/styles/main.css">
-	<link rel="stylesheet" href="../../root_/styles/fontawesome/css/all.min.css">
-	<script src="config.json"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/wavesurfer.js/2.0.4/wavesurfer.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/wavesurfer.js/2.0.4/plugin/wavesurfer.regions.min.js"></script>
-	<script src="../../root_/htmls/header.html"></script>
-	<script src="../../root_/htmls/body.html"></script>
-	<script src="../../root_/htmls/footer.html"></script>
-	<script src="../../root_/scripts/main.js"></script>
+	<script src="../../root_/scripts/App.js" onload='loadApp(`
+		<script src="../../root_/scripts/lang.js"></script>
+		<script src="../../root_/scripts/theme.js"></script>
+		<link rel="stylesheet" href="../../root_/styles/main.css">
+		<link rel="stylesheet" href="../../root_/styles/fontawesome/css/all.min.css">
+		<script src="config.json"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/wavesurfer.js/2.0.4/wavesurfer.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/wavesurfer.js/2.0.4/plugin/wavesurfer.regions.min.js"></script>
+		<script src="../../root_/htmls/header.html"></script>
+		<script src="../../root_/htmls/body.html"></script>
+		<script src="../../root_/htmls/footer.html"></script>
+		<script src="../../root_/scripts/main.js"></script>
+	`)'></script>
 	</head><body></body></html>'''
 
 def track_embed():
@@ -27,15 +29,17 @@ def track_embed():
 	<title>Zombi Music</title>
 	<meta name="viewport" content="width=device-width">
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<link rel="stylesheet" href="../../root_/styles/main.css">
-	<script src="../../root_/scripts/parse_params.js"></script>
-	<link rel="stylesheet" href="../../root_/styles/embed.css">
-	<link rel="stylesheet" href="../../root_/styles/fontawesome/css/all.min.css">
-	<script src="config.json"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/wavesurfer.js/2.0.4/wavesurfer.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/wavesurfer.js/2.0.4/plugin/wavesurfer.regions.min.js"></script>
-	<script src="../../root_/htmls/embed.html"></script>
-	<script src="../../root_/scripts/embed.js"></script>
+	<script src="../../root_/scripts/App.js" onload='loadApp(`
+		<link rel="stylesheet" href="../../root_/styles/main.css">
+		<script src="../../root_/scripts/parse_params.js"></script>
+		<link rel="stylesheet" href="../../root_/styles/embed.css">
+		<link rel="stylesheet" href="../../root_/styles/fontawesome/css/all.min.css">
+		<script src="config.json"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/wavesurfer.js/2.0.4/wavesurfer.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/wavesurfer.js/2.0.4/plugin/wavesurfer.regions.min.js"></script>
+		<script src="../../root_/htmls/embed.html"></script>
+		<script src="../../root_/scripts/embed.js"></script>
+	`)'></script>
 	</head><body></body></html>'''
 
 def atrist_config(name, image="../root_/images/people.svg"):
@@ -51,14 +55,16 @@ def artist_index(name, image="../root_/images/people.svg"):
 	<meta property="og:title" content="{name}">
 	<meta property="og:description" content="Artist">
 	<meta property="og:site_name" content="zombi.music">
-	<script src="../root_/scripts/lang.js"></script>
-	<script src="../root_/scripts/theme.js"></script>
-	<script src="artist.json"></script>
-	<script src="../root_/bd.json"></script>
-	<link rel="stylesheet" href="../root_/styles/fontawesome/css/all.min.css">
-	<link rel="stylesheet" href="../root_/styles/main.css">
-	<script src="../root_/htmls/header.html"></script>
-	<script src="../root_/htmls/body_artist.html"></script>
-	<script src="../root_/htmls/footer.html"></script>
-	<script src="../root_/scripts/artist.js"></script>
+	<script src="../root_/scripts/App.js" onload='loadApp(`
+		<script src="../root_/scripts/lang.js"></script>
+		<script src="../root_/scripts/theme.js"></script>
+		<script src="artist.json"></script>
+		<script src="../root_/bd.json"></script>
+		<link rel="stylesheet" href="../root_/styles/fontawesome/css/all.min.css">
+		<link rel="stylesheet" href="../root_/styles/main.css">
+		<script src="../root_/htmls/header.html"></script>
+		<script src="../root_/htmls/body_artist.html"></script>
+		<script src="../root_/htmls/footer.html"></script>
+		<script src="../root_/scripts/artist.js"></script>
+	`)'></script>
 	</head><body></body></html>'''
