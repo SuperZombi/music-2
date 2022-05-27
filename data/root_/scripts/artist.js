061642e6e1f4f9ac0138483ba23c609c47b525d6
Super Zombi <rost201541@gmail.com>
2022-05-27 15:16:35 +0300

    Update artist.js

--- data/root_/scripts/artist.js
+++ data/root_/scripts/artist.js
@@ -1,19 +1,18 @@
-window.onload = function() {
-	(function load_page(){
-	if (typeof header !== 'undefined' && typeof body !== 'undefined' && typeof footer !== 'undefined'){
-		document.body.innerHTML += header
-		document.body.innerHTML += body
-		document.body.innerHTML += footer
+(function load_page(){
+if (typeof header !== 'undefined' && typeof body !== 'undefined' && typeof footer !== 'undefined'){
+	document.body.innerHTML += header
+	document.body.innerHTML += body
+	document.body.innerHTML += footer
 
-		setTimeout(function(){document.body.style.transition = "1s"}, 500)
+	setTimeout(function(){document.body.style.transition = "1s"}, 500)
 
-		main()
-	}
-	else{
-		setTimeout(function(){load_page()}, 500)
-	}
-	})()
+	main()
 }
+else{
+	setTimeout(function(){load_page()}, 500)
+}
+})()
+
 window.onresize = function(){ overflowed() }
 window.orientationchange = function(){ overflowed() }
 window.onscroll = function(){showScrollTop()}
